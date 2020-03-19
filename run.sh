7cd0c85aae056bcf2d2f275f7f3d2cb7fa40f981
James Alexander Clark <james.clark@ligo.org>
2020-03-19 10:09:23 -0400

    updated run script slightly

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #/bin/bash -e
 FAHClient --team 234340 --user jclark_astro --passkey 1733b9ade0070dabf0b4c753ee9af252 \
-  --cpu-usage 100  --cycles 1000
+  --cpu-usage 100  --cpus 1 --cycles 10000 \
+  --exit-when-done  \
+  --checkpoint 1
+  #--log-redirect \
 
