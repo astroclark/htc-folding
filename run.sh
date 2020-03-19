17a253362efac8aabb3297dda9dc9edd2504f7aa
James Alexander Clark <james.clark@ligo.org>
2020-03-19 10:46:47 -0400

    enhancements to run script

--- run.sh
+++ run.sh
@@ -1,7 +1,17 @@
 #/bin/bash -e
+
+workdir=$1
+
+mkdir -p ${workdir}
+pushd ${workdir}
+
 FAHClient --team 234340 --user jclark_astro --passkey 1733b9ade0070dabf0b4c753ee9af252 \
-  --cpu-usage 100  --cpus 1 --cycles 10000 \
+  --cpu-usage 100  --cpus 1 --cycles 100 \
   --exit-when-done  \
   --checkpoint 1
   #--log-redirect \
 
+# Clean up any configuration data
+rm config.xml GPUs.txt
+
+popd
